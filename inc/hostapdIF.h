cb39a49c9fd9a66090c8493da4ef6f9a7f5224f4
naushada <naushad.dln@gmail.com>
2019-11-01 23:08:45 +0530

    licence Updated

--- inc/hostapdIF.h
+++ inc/hostapdIF.h
@@ -1,6 +1,25 @@
 #ifndef __HOSTAPD_IF_H__
 #define __HOSTAPD_IF_H__
 
+/*
+This file interfaces with ACE Framework library and provides the implementation
+for command/response to/from hostapd.
+Copyright (C) <2019>  <Mohammed Naushad Ahmed>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <ace/Reactor.h>
 #include <ace/Basic_Types.h>
 #include <ace/Event_Handler.h>
--- src/vapsh.cc
+++ src/vapsh.cc
@@ -1,6 +1,25 @@
 #ifndef __VAPSH_CC__
 #define __VAPSH_CC__
 
+/*
+This file interfaces with readlineIF and hostapdIF library and provides the implementation
+for command/response to/from hostapd.
+Copyright (C) <2019>  <Mohammed Naushad Ahmed>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "vapsh.h"
 
 
